46f4f6fe969f5341aea40edf26a2762d96853029
HuyNguyen <huuhuybn@gmail.com>
2019-06-19 13:38:03 +0700

    second commit

--- screen/GeoScreen.js
+++ screen/GeoScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
+import * as Location from 'expo-location';
 export default function GeoScreen() {
   return (
     <View style={styles.container}>
--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,10 +1,47 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
 
 export default function HomeScreen(props) {
 
-    const [username, setUsername] = useState('admin');
-    const [count, setCount] = useState(0);
+    state = {
+        location: null,
+        errorMessage: null,
+    };
+
+    const [location, setLocation] = useState();
+    const [errorMessage, setErrorMessage] = useState('Waiting...');
+
+    const [latitude, setLatitude] = useState();
+
+    const [longitude, setLongitude] = useState();
+
+    useEffect(() => {
+
+        if (Platform.OS === 'android' && !Constants.isDevice) {
+            setErrorMessage('Oops, this will not work on Sketch in an Android emulator. Try it on your device!');
+        } else {
+            _getLocationAsync();
+        }
+    });
+
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+
+            setErrorMessage('Permission to access location was denied');
+
+        }
+
+        let info = await Location.getCurrentPositionAsync({});
+
+
+        setLatitude(info.coords.latitude);
+
+        setLongitude(info.coords.longitude);
+
+    }
+
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
@@ -15,6 +52,10 @@ export default function HomeScreen(props) {
             <Button title='Login' onPress={() => {
                 props.navigation.navigate('Login');
             }} />
+
+            <Text> Error : {errorMessage}</Text>
+            <Text>{longitude} : {latitude}</Text>
+
 
 
 
